d664fc45bedc9624ef29c8d77b24f4679a66f25f
diegogcast <diego.guerrero1@catolica.edu.sv>
2026-01-24 08:30:29 -0600

    primer commit - contador de cifras significativas

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContadorCifrasSignificativas
+{
+    class Program
+    {
+    
+        static void Main()
+        {
+            Console.WriteLine("CONTADOR DE CIFRAS SIGNIFICATIVAS");
+            Console.Write("Ingrese un número: ");
+            string input = Console.ReadLine();
+
+            int count = ContarCifrasSignificativas(input);
+
+            Console.WriteLine($"Cifras significativas: {count}");
+            Console.ReadKey();
+        }
+
+        static int ContarCifrasSignificativas(string num)
+        {
+            num = num.Trim();
+
+            if (num.StartsWith("-") || num.StartsWith("+"))
+                num = num.Substring(1);
+
+            if (num.Contains("."))
+            {
+                num = num.TrimStart('0');
+                num = num.Replace(".", "");
+                return num.Length;
+            }
+            else
+            {
+                num = num.TrimStart('0');
+                num = num.TrimEnd('0');
+
+                return num.Length;
+            }
+        }
+    }
+}
